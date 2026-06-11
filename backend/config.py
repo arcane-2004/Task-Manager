e2007a7a013210a7bcb6dfca85583378d6d8c109
arcane_2004 <sumitkp153@gmail.com>
2026-06-11 21:23:07 +0530

    removed unnecessary files or code

--- backend/config.py
+++ backend/config.py
@@ -7,13 +7,4 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    MAIL_SERVER = "smtp.gmail.com"
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-
-    MAIL_USERNAME = os.getenv("MAIL_USERNAME")
-    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
-
-
-print("MAIL_USERNAME =", Config.MAIL_USERNAME)
-print("MAIL_PASSWORD =", Config.MAIL_PASSWORD)
+    
